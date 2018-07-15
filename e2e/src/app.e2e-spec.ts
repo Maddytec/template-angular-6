dcc53b9475fb9a9f3ca5aa57e8dbb4ee80c052d7
Madson Silva <maddytec@gmail.com>
2018-07-15 15:52:11 -0300

    Commit initial

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to location-app!');
+    expect(page.getParagraphText()).toEqual('Welcome to template-Angular-6-app!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to location-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to template-Angular-6-app!');
   }));
 });
